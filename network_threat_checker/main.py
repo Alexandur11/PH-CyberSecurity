8bb532fb2b5ff4c30a303ae06f54aedcd21f3814
Aleksandar Daskalov <133868603+Alexandur11@users.noreply.github.com>
2024-06-28 20:02:08 +0300

    Update main.py

--- network_threat_checker/main.py
+++ network_threat_checker/main.py
@@ -1,8 +1,6 @@
 from network_threat_checker.network import display_current_connections, display_historical_connections
 
 
-
-
 # 1. Active connections
 display_current_connections() # Call to display active connections
 
